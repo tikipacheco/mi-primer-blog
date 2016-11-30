7bddb6f6870946daa3db09d71b5fb9814a7a4db5
tiki <ricardopachecotoledo@gmail.com>
2016-11-30 16:01:46 -0300

    se agrego el dominio del sitio

--- mysite/settings.py
+++ mysite/settings.py
@@ -115,6 +115,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+ALLOWED_HOSTS=['http://ricardopacheco.pythonanywhere.com/']
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
